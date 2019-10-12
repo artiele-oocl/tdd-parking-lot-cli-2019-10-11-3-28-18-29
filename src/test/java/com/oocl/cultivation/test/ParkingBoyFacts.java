d64e1553588f5ceb98cae0e7c538c95c52f44e09
Lester Jan Artienda <artiele@oocl.com>
2019-10-12 20:23:22 +0800

    Pass additional tests ensuring SmartParkingBoy can do what ParkingBoy can

--- src/test/java/com/oocl/cultivation/test/ParkingBoyFacts.java
+++ src/test/java/com/oocl/cultivation/test/ParkingBoyFacts.java
@@ -183,4 +183,127 @@ class ParkingBoyFacts {
 
         assertSame(parkingLot1, smartParkingBoy.getCarParkingLotAssociation(myCar));
     }
+    @Test
+    void smartParkingBoy_should_return_ticket_when_parkingBoy_park_car() {
+        Car car = new Car();
+        ParkingLot parkingLot = new ParkingLot();
+        SmartParkingBoy smartParkingBoy = new SmartParkingBoy(parkingLot);
+
+        ParkingTicket ticket = smartParkingBoy.park(car);
+
+        assertNotNull(ticket);
+    }
+
+    @Test
+    void smartParkingBoy_should_return_car_when_parkingBoy_fetch_car() {
+        Car car = new Car();
+        ParkingLot parkingLot = new ParkingLot();
+        SmartParkingBoy smartParkingBoy = new SmartParkingBoy(parkingLot);
+        ParkingTicket ticket = smartParkingBoy.park(car);
+
+        Car fetchedCar = smartParkingBoy.fetch(ticket);
+
+        assertEquals(car, fetchedCar);
+    }
+
+    @Test
+    void smartParkingBoy_should_return_3_tickets_when_parkingBoy_park_3_cars() {
+        Car car1 = new Car();
+        Car car2 = new Car();
+        Car car3 = new Car();
+        ParkingLot parkingLot = new ParkingLot();
+
+        parkingLot.addCar(car1);
+        parkingLot.addCar(car2);
+        parkingLot.addCar(car3);
+        int actual = parkingLot.getCarsCount();
+
+        assertEquals(3, actual);
+    }
+
+    @Test
+    void smartParkingBoy_should_return_car_from_corresponding_ticket_when_parkingBoy_fetch_cars() {
+        Car myCar = new Car();
+        Car otherCar1 = new Car();
+        Car otherCar2 = new Car();
+        ParkingLot parkingLot = new ParkingLot();
+        SmartParkingBoy smartParkingBoy = new SmartParkingBoy(parkingLot);
+        smartParkingBoy.park(otherCar1);
+        smartParkingBoy.park(otherCar2);
+        ParkingTicket ticket = smartParkingBoy.park(myCar);
+
+        Car actual = smartParkingBoy.fetch(ticket);
+
+        assertSame(myCar, actual);
+    }
+
+    @Test
+    void smartParkingBoy_should_return_errMsg_when_null_ticket_provided() {
+        Car otherCar1 = new Car();
+        Car otherCar2 = new Car();
+        ParkingLot parkingLot = new ParkingLot();
+        SmartParkingBoy smartParkingBoy = new SmartParkingBoy(parkingLot);
+        smartParkingBoy.park(otherCar1);
+        smartParkingBoy.park(otherCar2);
+
+        assertThrows(NullPointerException.class, ()->smartParkingBoy.fetch(null), smartParkingBoy.getLastErrorMessage());
+    }
+
+    @Test
+    void smartParkingBoy_should_return_errMsg_when_ticket_has_already_been_used() {
+        Car myCar = new Car();
+        ParkingLot parkingLot = new ParkingLot();
+        SmartParkingBoy smartParkingBoy = new SmartParkingBoy(parkingLot);
+        ParkingTicket ticket = smartParkingBoy.park(myCar);
+        smartParkingBoy.fetch(ticket);
+
+        assertThrows(NullPointerException.class, ()->smartParkingBoy.fetch(ticket), smartParkingBoy.getLastErrorMessage());
+    }
+    @Test
+    void smartParkingBoy_should_return_errMsg_when_no_parking_space_available() {
+        ParkingLot parkingLot = new ParkingLot();
+        SmartParkingBoy smartParkingBoy = new SmartParkingBoy(parkingLot);
+        for (int i = 0; i < 10; i++) {
+            smartParkingBoy.park(new Car());
+        }
+
+        Car myCar = new Car();
+
+        assertThrows(NullPointerException.class, ()->smartParkingBoy.park(myCar), smartParkingBoy.getLastErrorMessage());
+    }
+    @Test
+    void smartParkingBoy_should_return_null_when_parkingboy_park_an_already_parked_car() {
+        ParkingLot parkingLot = new ParkingLot();
+        SmartParkingBoy smartParkingBoy = new SmartParkingBoy(parkingLot);
+
+        Car myCar = new Car();
+        smartParkingBoy.park(myCar);
+        ParkingTicket ticket = smartParkingBoy.park(myCar);
+
+        assertNull(ticket);
+    }
+    @Test
+    void smartParkingBoy_should_return_null_when_parkingboy_park_nonexistent_car() {
+        ParkingLot parkingLot = new ParkingLot();
+        SmartParkingBoy smartParkingBoy = new SmartParkingBoy(parkingLot);
+
+        smartParkingBoy.park(null);
+        ParkingTicket ticket = smartParkingBoy.park(null);
+
+        assertNull(ticket);
+    }
+    @Test
+    void smartParkingBoy_should_park_on_first_parking_lot_when_first_parking_lot_isnot_yet_full() {
+        ParkingLot parkingLot1 = new ParkingLot();
+        ParkingLot parkingLot2 = new ParkingLot();
+        List<ParkingLot> parkingLots = new ArrayList<>();
+        parkingLots.add(parkingLot1);
+        parkingLots.add(parkingLot2);
+        SmartParkingBoy smartParkingBoy = new SmartParkingBoy(parkingLots);
+
+        Car myCar = new Car();
+        smartParkingBoy.park(myCar);
+
+        assertSame(parkingLot1, smartParkingBoy.getCarParkingLotAssociation(myCar));
+    }
 }
